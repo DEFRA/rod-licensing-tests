84e1eda4b6199b2c6909c2e64a5254e388e0f82a
Phil Benson <phil.benson@defra.gov.uk>
2020-08-11 17:53:35 +0100

    Generate a date of birth making them any age between 17 and 64, having turned that age yesterday so that the generated file is valid as long as possible

--- packages/pocl-test-file-generator/src/generateRecord.js
+++ packages/pocl-test-file-generator/src/generateRecord.js
@@ -1,9 +1,18 @@
+import moment from 'moment'
 
 const getRandomElementFromArray = arr => arr[Math.floor(Math.random() * arr.length)]
 
-export default function generateRecord(addresses, names) {
+const randomInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const JUNIOR_MAX_AGE = 16
+const SENIOR_MIN_AGE = 65
+
+export default function generateRecord(names, addresses) {
   const name = getRandomElementFromArray(names)
   const a = getRandomElementFromArray(addresses)
+  const dateOfBirth = moment().subtract(randomInteger(JUNIOR_MAX_AGE + 1, SENIOR_MIN_AGE - 1), 'years').subtract(1, 'day')
 
   return {
     REC: {
@@ -19,7 +28,7 @@ export default function generateRecord(addresses, names) {
       },
       PERMIT_TYPE: 'Salmon 12 month 1 Rod Licence (Full)',
       SEASON: 2020,
-      DOB: '1961-03-01',
+      DOB: dateOfBirth.format('YYYY-MM-DD'),
       AMOUNT: 82,
       MARKETING_FLAG: false
     }
--- packages/pocl-test-file-generator/test/generateRecord.test.js
+++ packages/pocl-test-file-generator/test/generateRecord.test.js
@@ -1,9 +1,11 @@
 import generateRecord from '../src/generateRecord.js'
+import moment from 'moment'
+import sinon from 'sinon'
 
 describe('generate record tests', () => {
   it('gets a random name from provided name array', () => {
     const names = getNames()
-    const { REC: r } = generateRecord(getAddresses(), names)
+    const { REC: r } = generateRecord(names, getAddresses())
     expect(names.some(n => n.forename === r.LICENSEE_FORNAME && n.surname === r.LICENSEE_SURNAME)).to.be.true
   });
 
@@ -13,14 +15,14 @@ describe('generate record tests', () => {
     { addresses: [{ address1: "Hannah's Cottage", address3: '3' }], output: "Hannah's Cottage, 3, " }
   ]).forEach(test => {
     it('concatenates address1, address2 and address3 to make premises', () => {
-      const { REC: { LICENSEE_ADDRESS: { PREMISES: premises } }} = generateRecord(test.addresses, getNames())
+      const { REC: { LICENSEE_ADDRESS: { PREMISES: premises } }} = generateRecord(getNames(), test.addresses)
       expect(premises).to.equal(test.output)
     })    
   });
 
   it('gets a random address from provided address array', () => {
     const addresses = getAddresses()
-    const { REC: { LICENSEE_ADDRESS: la } } = generateRecord(addresses, getNames())
+    const { REC: { LICENSEE_ADDRESS: la } } = generateRecord(getNames(), addresses)
     expect(addresses.some(a => 
       la.PREMISES === [1,2,3].map(x => !!a[`address${x}`] ? `${a[`address${x}`]}, ` : '').join('') &&
       la.TOWN === a.address6 &&
@@ -29,9 +31,53 @@ describe('generate record tests', () => {
   })
 
   it('sets DD_REFERENCE as a six-digit random number', () => {
-    const { REC: { DD_REFERENCE }} = generateRecord(getAddresses(), getNames())
+    const { REC: { DD_REFERENCE }} = generateRecord(getNames(), getAddresses())
     expect(/^[0-9]{6}$/.test(DD_REFERENCE)).to.be.true
+  });
+
+// minor max age: 12
+// junior max age: 16
+// senior min age: 65
+
+  ([
+    { random: 0.999, expectedAge: 64 },
+    { random: 0.001, expectedAge: 17 },
+    { random: 0.438, expectedAge: 38 },
+    { random: 0.644, expectedAge: 47 },
+    { random: 0.925, expectedAge: 61 }
+  ]).forEach(test => {
+    it('generates a date of birth between 16 and 65 years old', () => {
+      try {
+        sinon.stub(Math, 'random').callsFake(() => test.random)
+        const { REC: { DOB } } = generateRecord(getNames(), getAddresses())
+        const age = moment().diff(moment(DOB, 'YYYY-MM-DD'), 'years')
+        expect(age).to.equal(test.expectedAge)
+      } finally {
+        Math.random.restore()
+      }
+    })
+  });
+
+  ([
+    { date: moment('2020-04-15') },
+    { date: moment('2020-01-01') }
+  ]).forEach(test => {
+    it('birthday is always yesterday', () => {
+      const clock = sinon.useFakeTimers({
+        now: test.date.valueOf()
+      })
+      try {
+        const { REC: { DOB } } = generateRecord(getNames(), getAddresses())
+        const birthday = moment(DOB, 'YYYY-MM-DD')
+        const yesterday = moment().subtract(1, 'day')
+        expect(yesterday.month()).to.equal(birthday.month(), 'birthday month is wrong')
+        expect(yesterday.date()).to.equal(birthday.date(), 'birthday date is wrong')
+      } finally {
+        clock.restore()
+      }
+    })    
   })
+
 })
 
 const getNames = () => [
