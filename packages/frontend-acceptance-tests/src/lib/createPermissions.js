dd55fdf24fde82290b2c327f17e59744725a8ca6
Phil Benson <phil.benson@defra.gov.uk>
2020-10-08 17:55:26 +0100

    Added issueDate, startDate, permitId and licensee fields

--- packages/frontend-acceptance-tests/src/lib/createPermissions.js
+++ packages/frontend-acceptance-tests/src/lib/createPermissions.js
@@ -1,3 +1,5 @@
+import uuid from './stubbableUuid.js'
+
 export const PERMISSION_EXPIRY = {
   YESTERDAY: -1,
   TODAY: 0,
@@ -19,7 +21,42 @@ const getEndDate = expiryDateSpec => {
 }
 
 export const createPermission = (expiryDateSpec = PERMISSION_EXPIRY.TODAY) => {
+  const endDate = getEndDate(expiryDateSpec)
+  const startDate = new Date(endDate)
+  startDate.setFullYear(startDate.getFullYear() - 1)
+  const birthDate = new Date()
+  birthDate.setDate(birthDate.getDate() - 1)
+  birthDate.setFullYear(birthDate.getFullYear() - 25)
   return {
-    endDate: getEndDate(expiryDateSpec).toISOString()
+    issueDate: startDate.toISOString(),
+    startDate: startDate.toISOString(),
+    endDate: getEndDate(expiryDateSpec).toISOString(),
+    permitId: uuid.v4(),
+    licensee: {
+      birthDate: `${birthDate.getFullYear()}-${birthDate.getMonth() + 1}-${birthDate.getDate()}`,
+      firstName: 'Fester',
+      lastName: 'Tester',
+      email: 'person@example.com',
+      mobilePhone: '+44 7700 900088',
+      premises: 'Example House',
+      street: 'Example Street',
+      locality: 'Near Sample',
+      town: 'Exampleton',
+      postcode: 'AB12 3CD',
+      country: 'GB',
+      preferredMethodOfConfirmation: 'Text',
+      preferredMethodOfNewsletter: 'Email',
+      preferredMethodOfReminder: 'Letter'
+    }
+    /* concessions: [
+      {
+        id: 'd0ece997-ef65-e611-80dc-c4346bad4004',
+        proof: {
+          type: 'National Insurance Number',
+          referenceNumber: 'AB 01 02 03 CD'
+        }
+      }
+    ], */
+    /* referenceNumber: '11100420-2WT1SFT-KPMW2C' */
   }
 }
--- packages/frontend-acceptance-tests/test/createPermissions.test.js
+++ packages/frontend-acceptance-tests/test/createPermissions.test.js
@@ -1,12 +1,19 @@
 import { createPermission, PERMISSION_EXPIRY } from '../src/lib/createPermissions.js'
 import sinon from 'sinon'
+import uuid from '../src/lib/stubbableUuid.js'
+
+console.log('UUID', uuid.v4())
 
 describe('createPermissions tests', () => {
-  [
-    { today: new Date(2020, 0, 1), tomorrow: '02/01/2020', yesterday: '31/12/2019' },
-    { today: new Date(2020, 9, 15), tomorrow: '16/10/2020', yesterday: '14/10/2020' },
-    { today: new Date(2020, 1, 29), tomorrow: '01/03/2020', yesterday: '28/02/2020' },
-    { today: new Date(2020, 2, 1), tomorrow: '02/03/2020', yesterday: '29/02/2020' }
+  ;[
+    {
+      today: new Date(2020, 0, 1, 9, 15, 22),
+      tomorrow: '2020-01-02T09:15:22.000Z',
+      yesterday: '2019-12-31T09:15:22.000Z'
+    },
+    { today: new Date(2020, 9, 15, 13, 28, 59), tomorrow: '2020-10-16T12:28:59.000Z', yesterday: '2020-10-14T12:28:59.000Z' },
+    { today: new Date(2020, 1, 29, 6, 59, 28), tomorrow: '2020-03-01T06:59:28.000Z', yesterday: '2020-02-28T06:59:28.000Z' },
+    { today: new Date(2020, 2, 1, 22, 1, 5), tomorrow: '2020-03-02T22:01:05.000Z', yesterday: '2020-02-29T22:01:05.000Z' }
   ].forEach(({ today, tomorrow, yesterday }) => {
     describe(`tests when today is ${today.toLocaleDateString()}`, () => {
       let mockClock
@@ -21,18 +28,93 @@ describe('createPermissions tests', () => {
 
       it('creates a permission that expires today', () => {
         const permission = createPermission()
-        expect(new Date(permission.endDate).toLocaleDateString()).to.equal(new Date().toLocaleDateString())
+        expect(permission.endDate).to.equal(today.toISOString())
       })
 
       it('creates a permission that expired yesterday', () => {
         const permission = createPermission(PERMISSION_EXPIRY.YESTERDAY)
-        expect(new Date(permission.endDate).toLocaleDateString()).to.equal(yesterday)
+        expect(permission.endDate).to.equal(yesterday)
       })
 
       it('creates a permission that expires tomorrow', () => {
         const permission = createPermission(PERMISSION_EXPIRY.TOMORROW)
-        expect(new Date(permission.endDate).toLocaleDateString()).to.equal(tomorrow)
+        expect(permission.endDate).to.equal(tomorrow)
       })
+
+      it('permission expiring yesterday started a year ago', () => {
+        const yearAndADayAgo = new Date(yesterday)
+        yearAndADayAgo.setFullYear(yearAndADayAgo.getFullYear() - 1)
+        const permission = createPermission(PERMISSION_EXPIRY.YESTERDAY)
+        expect(permission.startDate).to.equal(yearAndADayAgo.toISOString())
+      })
+
+      it('permission expiring today started a year ago', () => {
+        const yearAgo = new Date(today)
+        yearAgo.setFullYear(yearAgo.getFullYear() - 1)
+        const permission = createPermission()
+        expect(permission.startDate).to.equal(yearAgo.toISOString())
+      })
+
+      it('permission expiring tomorrow started a year ago', () => {
+        const yearAgoTomorrow = new Date(tomorrow)
+        yearAgoTomorrow.setFullYear(yearAgoTomorrow.getFullYear() - 1)
+        const permission = createPermission(PERMISSION_EXPIRY.TOMORROW)
+        expect(permission.startDate).to.equal(yearAgoTomorrow.toISOString())
+      })
+
+      it('sets issue date to equal start date', () => {
+        const permission = createPermission()
+        expect(permission.startDate).to.equal(permission.issueDate)
+      })
+
+      it('sets licensee date of birth to be 25 years ago yesterday', () => {
+        const permission = createPermission()
+        const dateOfBirth = new Date(yesterday)
+        dateOfBirth.setFullYear(dateOfBirth.getFullYear() - 25)
+        expect(permission.licensee.birthDate).to.equal(
+          `${dateOfBirth.getFullYear()}-${dateOfBirth.getMonth() + 1}-${dateOfBirth.getDate()}`
+        )
+      })
+    })
+  })
+
+  it('generates a permit id using uuid', () => {
+    const sampleUUID = 'abcdefgh-ijkl-mnop-qrst-uvwxyz012345'
+    const uuidStub = sinon.stub(uuid, 'v4')
+    uuidStub.onFirstCall().returns(sampleUUID)
+
+    try {
+      expect(createPermission().permitId).to.equal(sampleUUID)
+    } finally {
+      uuidStub.restore()
+    }
+  })
+
+  const nameMatcher = /[A-Za-z]{3,}/
+  const addressMatcher = /([A-Za-z0-9-]{2,}\s)*/
+  const contactMatcher = /(Text|Email|Letter)/
+
+  ;[
+    ['firstName', nameMatcher],
+    ['lastName', nameMatcher],
+    ['email', /[A-Za-z0-9.]{3,}@[A-Za-z0-9]{3,}\.[A-Za-z]{2,}([.A-Za-z]{2,})*/],
+    ['mobilePhone', /(\+[0-9]{2}\s|0)[0-9\s]*/],
+    ['premises', /([0-9]|[A-Za-z\s])*/],
+    ['street', addressMatcher],
+    ['locality', addressMatcher],
+    ['town', addressMatcher],
+    ['postcode', /[A-Za-z]{1,2}[0-9]{1,2}\s[0-9]{1}[A-Za-z]{2}/],
+    ['country', /[A-Za-z]{2}/],
+    ['preferredMethodOfConfirmation', contactMatcher],
+    ['preferredMethodOfNewsletter', contactMatcher],
+    ['preferredMethodOfReminder', contactMatcher]
+  ].forEach(([fieldName, matcher]) => {
+    it(`licensee has ${fieldName} matching expected format`, () => {
+      const permission = createPermission()
+      console.log(fieldName, permission[fieldName])
+      const matches = matcher.test(permission.licensee[fieldName])
+      // eslint-disable-next-line no-unused-expressions
+      expect(matches).to.be.true
     })
   })
 })
