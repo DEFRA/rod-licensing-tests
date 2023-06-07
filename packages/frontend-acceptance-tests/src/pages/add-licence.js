895a1335ca0c871da59838deb5bc84453a635de7
naimullakhandefra <77017471+naimullakhandefra@users.noreply.github.com>
2023-06-07 11:01:18 +0100

    fix lint

--- packages/frontend-acceptance-tests/src/pages/add-licence.js
+++ packages/frontend-acceptance-tests/src/pages/add-licence.js
@@ -7,7 +7,7 @@ class AddLicence extends Page {
   async setDoNotAddLicence () {
     await $('#selector-no').click()
     logger.info(`No, lets finish selected`)
-    }
+  }
 }
 
 module.exports = new AddLicence('/buy/add-licence')
--- packages/frontend-acceptance-tests/src/step-definitions/Add-Licence/I_am_on_the_add_licence_page_and_I_proceed_with_no.js
+++ packages/frontend-acceptance-tests/src/step-definitions/Add-Licence/I_am_on_the_add_licence_page_and_I_proceed_with_no.js
@@ -1,7 +1,7 @@
 const { defineStep } = require('@cucumber/cucumber')
 const AddLicencePage = require('../../pages/add-licence')
 
-defineStep('I am on the add licence page and I proceed with no', async ()  => {
+defineStep('I am on the add licence page and I proceed with no', async () => {
   await AddLicencePage.checkUrl()
   await AddLicencePage.setDoNotAddLicence()
   await AddLicencePage.continue()
