11ae1f517d5ac14627be5134b64d0487317adca8
irisfaraway <iris.faraway@gmail.com>
2023-09-28 16:39:32 +0100

    Also set cardholder name

--- packages/frontend-acceptance-tests/src/step-definitions/Gov-pay/I_enter_payment_and_addess_details_and_click_continue.js
+++ packages/frontend-acceptance-tests/src/step-definitions/Gov-pay/I_enter_payment_and_addess_details_and_click_continue.js
@@ -12,6 +12,7 @@ defineStep('I enter payment and address details', async () => {
   await GovPayPage.setCardNumber('4444333322221111')
   await GovPayPage.setCardExpiryMonth('01')
   await GovPayPage.setCardExpiryYear(futureYear)
+  await GovPayPage.setCardHolderName('Cardholder Name')
   await GovPayPage.setCvc('555')
   await GovPayPage.setAddressLine1('Horizon House')
   await GovPayPage.setAddressCity('Bristol')
