183070c3cbbacd0419d26383af711088dcb7294b
Phil Benson <phil.benson@defra.gov.uk>
2026-02-12 15:46:12 +0000

    Remove RCP feature switch (#301)
    * Remove RCP feature switch
    
    https://eaflood.atlassian.net/browse/IWTF-4977
    
    RCP is switched on now, so the feature switch can be removed and RCP tests set to always run

--- packages/frontend-acceptance-tests/src/step-definitions/Recurring-Payment/I_am_on_the_choose_payment_page_and_select_###.js
+++ packages/frontend-acceptance-tests/src/step-definitions/Recurring-Payment/I_am_on_the_choose_payment_page_and_select_###.js
@@ -3,24 +3,14 @@
 import { defineStep } from '@cucumber/cucumber'
 import ChoosePaymentPage from '../../pages/choose-payment.js'
 
-const recurringPayments = process.env.SHOW_RECURRING_PAYMENTS === 'true'
-
 defineStep('I select single licence only and click continue', async () => {
-    if (recurringPayments) {
-        await ChoosePaymentPage.checkUrl()
-        await ChoosePaymentPage.setChosenPayment('no')
-        await ChoosePaymentPage.continue()
-    } else {
-        return
-    }
+    await ChoosePaymentPage.checkUrl()
+    await ChoosePaymentPage.setChosenPayment('no')
+    await ChoosePaymentPage.continue()
 })
 
 defineStep('I select recurring payment and click continue', async () => {
-    if (recurringPayments) {
-        await ChoosePaymentPage.checkUrl()
-        await ChoosePaymentPage.setChosenPayment('yes')
-        await ChoosePaymentPage.continue()
-    } else {
-        return
-    }
+    await ChoosePaymentPage.checkUrl()
+    await ChoosePaymentPage.setChosenPayment('yes')
+    await ChoosePaymentPage.continue()
 })
